a7238d650e4697259634d3e1e39576d42208ec31
matus_jurecka <matus.jurecka@gmail.com>
2022-02-10 17:59:49 +0100

    common

--- control.c
+++ control.c
@@ -12,10 +12,10 @@
 #define ARROW           0x001b5b00
 #define CTRL_ARROW      0x313b3500
 #define SHIFT_ARROW     0x313b3200
-#define UP      0x41
-#define DOWN    0x42
-#define RIGHT   0x43
-#define LEFT    0x44
+#define UP      		0x41
+#define DOWN    		0x42
+#define RIGHT   		0x43
+#define LEFT    		0x44
 
 
 int checkKeyPress(appData *userData)
